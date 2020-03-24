0204a0057721aaad585bb3f62b771cabf18a6573
rmurgu <no-reply>
2020-03-24 16:45:31 +0200

    Populate job state through setters

--- src/main/java/schedulers/BankTransaction.java
+++ src/main/java/schedulers/BankTransaction.java
@@ -3,15 +3,32 @@ package schedulers;
 import org.quartz.*;
 
 public class BankTransaction implements Job {
+
+    String from;
+    String to;
+    String currency;
+    double sum;
+
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
         JobDataMap dataMap = context.getJobDetail().getJobDataMap();
 
-        String from = dataMap.getString("from");
-        String to = dataMap.getString("to");
-        String currency = dataMap.getString("currency");
-        double sum = dataMap.getDouble("sum");
-
         System.out.println(from + " sent " + currency + sum + " to " + to);
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public void setSum(double sum) {
+        this.sum = sum;
     }
 }
